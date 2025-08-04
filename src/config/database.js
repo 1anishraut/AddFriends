91f82001c8159ac9707a7ad6b9b00b59022105fb
Anish Raut <anishraut.555@gmail.com>
2025-08-04 22:21:22 +0530

    Pass updated

--- src/config/database.js
+++ src/config/database.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose")
 
 const connectDB = async () => {
     await mongoose.connect(
-      "mongodb+srv://anishraut555:Raut2000@nodejs-project01.s9u1dpk.mongodb.net/devTinder"
+      "mongodb+srv://anishraut555:KGW5v4qNR9qy8Tf4@nodejs-project01.s9u1dpk.mongodb.net/devTinder"
     );
 }
 
